4447bb62458d599ec29e809f3d6c71a58e2e1aea
Talha Ansari <131787244+talhaa1511@users.noreply.github.com>
2024-01-14 01:12:57 +0500

    Update reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -8,8 +8,6 @@ export const initialState = {
   top_artists: null,
   playing: false,
   item: null,
-  token:
-  "BQDSaP5m2VHw5TG3DjoNpP5zSpqUB-oYOz0C0ZSaDB2qmypgiJ3Omyr33Wx1rkymzO7segzcsgnZclf3v5idOpCfMNw7eQy9W5VNFlepBtoXELs2gYj4Al90Tjje00ZuPeR3NJt9pOS6IpIdc4HqaQEOAN4WE8c0fdm2f1VvGzwevgFeI1rxfaUawZu9k_omeSG2R9_6dRalyZ59cFjbkd90ya_Hpv5SxDHMTreeoPYAVJEYNZNJp4mKJJwSWolIhSjtPnxNiDqJponifdpx1vBg"
   
 };
 
